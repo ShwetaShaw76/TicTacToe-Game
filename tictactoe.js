01e62133deb21bb3d94593cfe6b410191caa8fbf
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2025-01-31 19:19:36 +0000

    js added

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,76 @@
 let boxes=document.querySelectorAll(".boxes");
 let resetbtn=document.querySelector(".restart");
-let name1=prompt
+let name1=prompt("Player X's Name");
+let name2=prompt("Player O's Name");
+let para=document.querySelector(".para");
+para.innerText=name2+"'s Turn";
+let turnO=true;
+
+const winning=[
+    [0,3,6],
+    [0,1,2],
+    [3,4,5],
+    [1,4,7],
+    [6,7,8],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+            para.innerText=name1+"'s Turn"
+        }
+        else{
+            box.innerText="X";
+            turnO=false;
+            para.innerText=name2+"'s Turn";
+        }
+        box.disabled=true;
+
+        checkWinner();
+    })
+})
+
+const disableboxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+const enableboxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+    }
+}
+
+const checkWinner=()=>{
+    for(arr of winning){
+        let pos1=boxes[arr[0]].innerText;
+        let pos2=boxes[arr[1]].innerText;
+        let pos3=boxes[arr[2]].innerText;
+
+        if(pos1!="" && pos2!="" && pos3!=""){
+            if(pos1==pos2 && pos2==pos3 && turnO==true){
+                para.innerText=name1+" Wins";
+                disableboxes();
+            }
+            else if(pos1==pos2 && pos2==pos3){
+                para.innerText=name2+" Wins";
+                disableboxes();
+            }
+        }
+    }
+}
+
+resetbtn.addEventListener("click",()=>{
+    for(let box of boxes){
+        box.innerText="";
+    }
+    enableboxes();
+    para.innerText=name2+"'s Turn";
+    turnO=true;
+})
